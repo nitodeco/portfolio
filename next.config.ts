b084cb46279c992614f97645a3dae6b43ff278ab
nitodeco <n.moehn@outlook.com>
2025-08-17 03:33:55 +0200

    feat: add language select

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 import type { NextConfig } from 'next';
 
-const withNextIntl = createNextIntlPlugin({
-  experimental: {
-    createMessagesDeclaration: './src/messages/en.json',
-  },
-});
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {};
 
---
+++ src/components/molecules/LocaleSelect/LocaleSelect.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import * as React from 'react';
+
+import { hasLocale, useLocale, useTranslations } from 'next-intl';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+import { usePathname, useRouter } from '@/i18n/navigation';
+import { routing } from '@/i18n/routing';
+
+export const LocaleSelect: React.FC = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+  const t = useTranslations('localeSelect');
+
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Select
+      value={locale}
+      onValueChange={(nextLocale) => {
+        if (hasLocale(routing.locales, nextLocale)) {
+          router.replace(pathname, { locale: nextLocale });
+          setOpen(false);
+        }
+      }}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <SelectTrigger className='cursor-pointer'>
+        <SelectValue placeholder={t('placeholder')} />
+      </SelectTrigger>
+      <SelectContent>
+        {routing.locales.map((opt) => (
+          <SelectItem key={opt} value={opt} className='cursor-pointer'>
+            {t(`locales.${opt}`)}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
