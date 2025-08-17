3e065576429e5b489d492b7a682a249869259e4e
nitodeco <n.moehn@outlook.com>
2025-08-17 22:29:51 +0200

    feat: add skill badge with popover

---
+++ src/components/molecules/SkillBadge/SkillBadge.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Badge } from '@/components/ui/badge';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+
+type Props = {
+  skill: string;
+  description: string;
+};
+
+export const SkillBadge: React.FC<Props> = ({ skill, description }) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Badge className='border-foreground bg-background text-foreground hover:bg-secondary hover:scale-102 hover:rotate-3 transition-transform ease-in-out duration-100 cursor-pointer'>
+          {skill}
+        </Badge>
+      </PopoverTrigger>
+      <PopoverContent>
+        <span className='text-sm'>{description}</span>
+      </PopoverContent>
+    </Popover>
+  );
+};
