903dfd6e7570dda2d179f173215af24679342957
nitodeco <n.moehn@outlook.com>
2025-08-17 04:01:16 +0200

    style: make header fixed

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -5,7 +5,7 @@ import { LocaleSelect } from '@/components/molecules/LocaleSelect';
 export const Header: React.FC = () => {
   return (
     <>
-      <header className='h-16 w-screen flex flex-row items-center justify-between p-8'>
+      <header className='h-16 w-screen flex flex-row items-center justify-between p-8 fixed'>
         <div></div>
         <LocaleSelect />
       </header>
