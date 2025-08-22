228f7992a0a6e660d2cdc6a7cd9b4f3597025ca2
nitodeco <n.moehn@outlook.com>
2025-08-22 23:43:38 +0200

    fix: change ph endpoint in instrumentation

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,8 +1,8 @@
 import posthog from 'posthog-js';
 
-if (process.env.NEXT_PUBLIC_POSTHOG_KEY && typeof window !== 'undefined') {
+if (process.env.NEXT_PUBLIC_POSTHOG_KEY! && typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: '/ingest',
+    api_host: '/relay-portfolio',
     ui_host: 'https://eu.posthog.com',
     defaults: '2025-05-24',
     capture_exceptions: true,
